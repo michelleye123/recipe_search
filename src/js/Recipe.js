bde9a5044383431f42c3f2d9811c09eaf1610f49
Michelle Ye <michelle.ye123@gmail.com>
2020-05-01 21:03:24 +1000

    clean up

--- src/js/Recipe.js
+++ src/js/Recipe.js
@@ -1,18 +1,4 @@
-/*
-how forkify-api differs from food2fork api:
-- no API key required
-- no proxy required
-- url = forkify-api.herokuapp.com;
-
-Recipe.js
-replace:
-const res = await axios(`${PROXY}http://food2fork.com/api/get?key=${KEY}&rId=${this.id}`);
-with:
-//const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
-
-*/
-
-import axios from 'axios';
+simport axios from 'axios';
 
 export default class Recipe {
     constructor(id) {
